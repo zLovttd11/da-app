5f2a6c4294c522279d3e2cb09e06e418863cc94d
zLovttd11 <siyuan.zeng@student.uts.edu.au>
2026-06-11 13:58:41 +0800

    v2 upgrade: modules/__init__.py

--- modules/__init__.py
+++ modules/__init__.py
@@ -1,9 +1,13 @@
-from .data_loader import load_data, detect_encoding, infer_column_types
+﻿from .data_loader import load_data, detect_encoding, infer_column_types
 from .data_profiler import profile_data
 from .descriptive import descriptive_stats, categorical_summary
 from .visualization import generate_plots, generate_static_charts
 from .correlation import correlation_analysis
-from .regression import regression_analysis
+from .regression import regression_analysis, regression_compare_models
 from .hypothesis import hypothesis_tests
+from .classification import classify_compare_models
+from .clustering import cluster_analysis
+from .feature_engineering import feature_importance_analysis
+from .cross_validation import split_data, kfold_cross_validation, stratified_kfold
 from .tableau_exporter import export_to_hyper
 from .report_generator import generate_report
