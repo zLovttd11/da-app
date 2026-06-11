f4c6e186952370ce85d3f60d67795435592aba05
zLovttd11 <siyuan.zeng@student.uts.edu.au>
2026-06-11 13:02:51 +0800

    Add modules/__init__.py

---
+++ modules/__init__.py
@@ -0,0 +1,9 @@
+from .data_loader import load_data, detect_encoding, infer_column_types
+from .data_profiler import profile_data
+from .descriptive import descriptive_stats, categorical_summary
+from .visualization import generate_plots, generate_static_charts
+from .correlation import correlation_analysis
+from .regression import regression_analysis
+from .hypothesis import hypothesis_tests
+from .tableau_exporter import export_to_hyper
+from .report_generator import generate_report
